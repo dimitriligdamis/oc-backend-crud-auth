501bda76331d9389bca0e4cd8eeebfdde4747a46
Dimitri Ligdamis <ligdamisdimitri@gmail.com>
2023-04-10 15:58:52 +0200

    :rocket: feat: Connexion a la base de données Mongo

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbPassword = process.env.MONGODB_PASSWORD;
+
+mongoose
+  .connect(
+    `mongodb+srv://ocfullstack:${dbPassword}@cluster0.qwsi739.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -15,7 +29,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/stuff", (req, res, next) => {
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet crée",
+  });
+});
+
+app.get("/api/stuff", (req, res, next) => {
   const stuff = [
     {
       _id: "oeihfzeoi",
